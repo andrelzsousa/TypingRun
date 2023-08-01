190fcb661a8928762be7c768b0641f561e1d0c1e
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2023-08-01 13:48:13 -0300

    Feat: typing logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+
+
 
 function App() {
+  const lorem = "non lorem veniam duis et labore nisi."
+  const textObj = lorem.split('').map((c, i) => {
+    return {char: c, style: 'default', i}
+  })
+  
+  const [text, setText] = useState(textObj)
+  const target = useRef(0)
+
+  useEffect(() => {
+    function checkKey(curIndex, keyPressed){
+        if(keyPressed === "Backspace"){
+          setText((items) => items.map((item) => {
+            return item.i === curIndex ? { ...item, style: 'default' } : item
+          }))
+          return
+        }
+
+        setText((items) => items.map((item) => {
+          return item.i === curIndex ? { ...item, style: text.at(curIndex).char === keyPressed ? 'hit' : 'miss' } : item
+        }))
+      
+    }
+
+    function callback(event){
+      if(event.key === "Backspace"){
+        target.current--
+        checkKey(target.current, event.key)
+      }
+      else{
+        checkKey(target.current, event.key)
+        target.current++
+      }
+    }
+
+    document.addEventListener("keydown", callback)
+
+    return () => {document.removeEventListener("keydown", callback)}
+  }, [text, setText])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <p>{text.map(c => <span className={c.style} key={c.i}>{c.char}</span>)}</p>
+  </div>
   );
 }
 
